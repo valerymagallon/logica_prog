a6f5a339e3efd25a3f3b7f9170bd155815524db3
valerymagallon <101846216+valerymagallon@users.noreply.github.com>
2022-03-17 19:49:56 -0600

    Actualización

---
+++ Python/HolaMundo.py
@@ -0,0 +1,7 @@
+# COMENTARIO: Ejemplo 1 Saludo
+# Los comentarios sirven para documentar y anclar cada parte
+# del programa en Python
+
+print("HOLA MUNDO")
+print("VALERY PAOLA MAGALLON VALENCIA");
+print(10+20)
