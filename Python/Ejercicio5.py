caf428615ab5684ec0d869516862e2d92f399864
valerymagallon <101846216+valerymagallon@users.noreply.github.com>
2022-03-24 20:15:34 -0600

    Ejercicios

---
+++ Python/Ejercicio5.py
@@ -0,0 +1,10 @@
+#Ejercicio 5
+
+grados = float(input("Ingresa los grados: "))
+
+kelvin = grados + 273.15
+farenheit = grados * (9/5)+32
+
+print(f"Grados celsius: {grados}")
+print(f"Grados kelvin: {kelvin}")
+print(f"Grados farenheit: {farenheit}")
