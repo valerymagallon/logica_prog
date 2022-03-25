b33b2d34c479abd60ee8b67e4ef5abfd9700cb95
valerymagallon <101846216+valerymagallon@users.noreply.github.com>
2022-03-24 19:56:33 -0600

    Ejercicios

--- Python/Ejercicio1.py
+++ Python/Ejercicio1.py
@@ -6,7 +6,7 @@ import math
 
 # ENTRADA DE DATOS
 
-número1 = float(input("Ingresa la primera calificación: ")) # Convertir de string a int
+número1 = float(input("Ingresa la primera calificación: ")) 
 número2 = float(input("Ingresa la segunda calificación: "))
 número3 = float(input("Ingresa la tercera calificación: "))
 
@@ -14,6 +14,12 @@ número3 = float(input("Ingresa la tercera calificación: "))
 
 promedio = (número1 + número2 + número3)/3 
 
+
 # SALIDA DE DATOS 
 
 print(f"El promedio de 3 calificaciones es = {promedio}")
+
+if (promedio>=6.1):
+    print("Aprobado")
+elif(promedio<6):
+    print("Reprobado")
