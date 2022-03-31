c54023ae3d5e849048eaca148d65f8ef711218e5
valerymagallon <101846216+valerymagallon@users.noreply.github.com>
2022-03-30 18:31:53 -0600

    Ejercicios

---
+++ Python/Ejercicio11.py
@@ -0,0 +1,9 @@
+# Ejercicio 11
+
+arreglo_lista = [8, 25, 4, 97, 1011, 55, 74, 1, 140, 30]
+#Índice (Index)  0   1  2   3    4    5   6   7  8    9
+
+arreglo_lista.sort()
+
+#SALIDA
+print(arreglo_lista)
